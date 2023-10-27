d586b71a7ac90a2c3e019c2074be5dbf648f04bc
bagusbangselok <bagusbangselok@gmail.com>
2023-10-27 22:53:22 +0700

    update list request di home guru

--- lib/ui/home_teacher/home_teacher_request.dart
+++ lib/ui/home_teacher/home_teacher_request.dart
@@ -19,7 +19,8 @@ class HomeTeacherRequest extends StatefulWidget {
 class _HomeTeacherRequestState extends State<HomeTeacherRequest> {
   bool showCardListUser = true;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  ProfileTeacherController _profileTeacherController = ProfileTeacherController();
+  ProfileTeacherController _profileTeacherController =
+      ProfileTeacherController();
   List<TeacherModel> items = [];
 
   @override
@@ -29,194 +30,200 @@ class _HomeTeacherRequestState extends State<HomeTeacherRequest> {
       drawer: DrawerTeacher(),
       backgroundColor: primaryColor,
       body: FutureBuilder<UserModel?>(
-        future: _profileTeacherController.getProfileByToken(),
-        builder: (BuildContext context, AsyncSnapshot<UserModel?> snapshot) {
-          print("data : ${snapshot.data}");
-          return SingleChildScrollView(
-              child: Column(children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        icon: const Icon(
-                          Icons.menu,
-                          color: whiteColor,
-                          size: 30, // Changing Drawer Icon Size
+          future: _profileTeacherController.getProfileByToken(),
+          builder: (BuildContext context, AsyncSnapshot<UserModel?> snapshot) {
+            print("data : ${snapshot.data}");
+            return SingleChildScrollView(
+                child: Column(children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: const Icon(
+                            Icons.menu,
+                            color: whiteColor,
+                            size: 30, // Changing Drawer Icon Size
+                          ),
+                          onPressed: () {
+                            _scaffoldKey.currentState?.openDrawer();
+                          },
+                          tooltip: MaterialLocalizations.of(context)
+                              .openAppDrawerTooltip,
                         ),
-                        onPressed: () {
-                          _scaffoldKey.currentState?.openDrawer();
-                        },
-                        tooltip:
-                            MaterialLocalizations.of(context).openAppDrawerTooltip,
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Image.asset("assets/icon/icon_white.png", width: 50),
-                        borderRadius: BorderRadius.circular(50),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    children: [
-                      Flexible(
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 40, left: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("${snapshot.data?.name}",
-                                  style: TextStyle(
-                                      color: whiteColor,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w600)),
-                              SizedBox(height: 12),
-                              Row()
-                            ],
+                        InkWell(
+                          onTap: () {},
+                          child: Image.asset("assets/icon/icon_white.png",
+                              width: 50),
+                          borderRadius: BorderRadius.circular(50),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 40, left: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("${snapshot.data?.name}",
+                                    style: TextStyle(
+                                        color: whiteColor,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600)),
+                                SizedBox(height: 12),
+                                Row()
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomeTeacherSchedule()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: whiteColor,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 12, vertical: 10),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: Text(
-                          "Data Jadwal",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            color: secondaryColor,
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 12),
-                      ElevatedButton(
-                          onPressed: () {},
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeTeacherSchedule()),
+                            );
+                          },
                           style: ElevatedButton.styleFrom(
-                            primary: secondaryColor,
+                            primary: whiteColor,
                             padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 10),
+                                horizontal: 12, vertical: 10),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             ),
                           ),
-                          child: Row(children: [
-                            Icon(
-                              Icons.person,
-                              color: whiteColor,
+                          child: Text(
+                            "Data Jadwal",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: secondaryColor,
                             ),
-                            SizedBox(width: 4),
-                            Text("User Request",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w700,
-                                  color: whiteColor,
-                                ))
-                          ])),
-                    ],
-                  )
-                ],
+                          ),
+                        ),
+                        SizedBox(width: 12),
+                        ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              primary: secondaryColor,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Row(children: [
+                              Icon(
+                                Icons.person,
+                                color: whiteColor,
+                              ),
+                              SizedBox(width: 4),
+                              Text("User Request",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                    color: whiteColor,
+                                  ))
+                            ])),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            // Container list siswa
-            SizedBox(
-                width: double.infinity,
-                child: Container(
-                  padding: EdgeInsets.only(left: 20, right: 20, top: 44),
+              // Container list siswa
+              SizedBox(
                   width: double.infinity,
-                  height: 540,
-                  decoration: BoxDecoration(
-                      color: whiteColor,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(40),
-                          topRight: Radius.circular(40)),
-                      shape: BoxShape.rectangle),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "List User Request",
-                        style: TextStyle(
-                            color: blackColor,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(height: 20),
-                      // Conditional
-                      !snapshot.hasData ? Expanded(
-                        child: Center(
-                          // render the loading indicator
-                            child: CircularProgressIndicator()),
-                      ) : listRequest(context)
-                    ],
-                  ),
-                ))
-          ]));
-        }
-      ),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 20, right: 20, top: 44),
+                    width: double.infinity,
+                    height: 540,
+                    decoration: BoxDecoration(
+                        color: whiteColor,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(40),
+                            topRight: Radius.circular(40)),
+                        shape: BoxShape.rectangle),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "List User Request",
+                          style: TextStyle(
+                              color: blackColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(height: 20),
+                        // Conditional
+                        !snapshot.hasData
+                            ? Expanded(
+                                child: Center(
+                                    // render the loading indicator
+                                    child: CircularProgressIndicator()),
+                              )
+                            : listRequest(context)
+                      ],
+                    ),
+                  ))
+            ]));
+          }),
     );
   }
 
-  Widget listRequest(BuildContext context){
+  Widget listRequest(BuildContext context) {
     return FutureBuilder<List<PesananModel>>(
-      future: _profileTeacherController.getListPesananGuru(),
-      builder: (BuildContext context, AsyncSnapshot<List<PesananModel>> snapshotPesanan) {
-        print('dataaa : ${snapshotPesanan.data}');
-        return !snapshotPesanan.hasData ?
-            Expanded(
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            ) :
-        Expanded(
-          child: ListView.builder(
-              itemCount: snapshotPesanan.data!.length,
-              itemBuilder: (BuildContext context, int index) {
-                return SingleChildScrollView(
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DetailHomeRequest()),
-                      );
-                    },
-                    child: CardListUser(
-                      urlImage: "assets/icon/user_icon1.png",
-                      name: "${snapshotPesanan.data![index].murid.name}",
-                      level: "${snapshotPesanan.data![index].murid.jenjangId}",
-                      subject: "${snapshotPesanan.data![index].jadwal.mataPelajaranId}",
-                      time: "${snapshotPesanan.data![index].jadwal.waktuMulai} - ${snapshotPesanan.data![index].jadwal.waktuAkhir}",
-                    ),
+        future: _profileTeacherController.getListPesananGuru(),
+        builder: (BuildContext context,
+            AsyncSnapshot<List<PesananModel>> snapshotPesanan) {
+          print('dataaa : ${snapshotPesanan.data}');
+          return !snapshotPesanan.hasData
+              ? Expanded(
+                  child: Center(
+                    child: CircularProgressIndicator(),
                   ),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                      itemCount: snapshotPesanan.data!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SingleChildScrollView(
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => DetailHomeRequest()),
+                              );
+                            },
+                            child: CardListUser(
+                              urlImage: "assets/icon/user_icon1.png",
+                              name:
+                                  "${snapshotPesanan.data![index].murid.name}",
+                              level:
+                                  "${snapshotPesanan.data![index].murid.jenjangId}",
+                              subject:
+                                  "${snapshotPesanan.data![index].jadwal.mataPelajaranId}",
+                              time:
+                                  "${snapshotPesanan.data![index].jadwal.waktuMulai} - "
+                                      "${snapshotPesanan.data![index].jadwal.waktuAkhir}",
+                            ),
+                          ),
+                        );
+                      }),
                 );
-              }
-          ),
-        );
-      }
-    );
+        });
   }
 }
